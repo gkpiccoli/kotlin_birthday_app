819ba900a2fbe623300e130085c6f85e8f4b6dd3
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-02-14 16:39:58 -0300

    FEAT: add image background

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -10,6 +12,8 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
@@ -45,14 +49,19 @@ fun BirthDayGreetingWithText(message: String, from: String){
 
 @Composable
 fun BirthdayGreetingWithImage(message: String, from: String){
+    val image = painterResource(id = R.drawable.androidparty);
+  Box {
 
+      Image(painter = image, contentDescription = null )
+      
+  }
 }
 
 
-@Preview(showBackground = true, name = "My Preview")
+@Preview(showBackground = false)
 @Composable
 fun BirthDayGreetingWithTextPreview() {
     HappyBirthdayTheme {
-        BirthDayGreetingWithText(message = "Happy Birthday Gustavo!", from = "- from Android.")
+        BirthdayGreetingWithImage(message = "Happy Birthday Gustavo!", from = "- from Android.")
     }
 }
