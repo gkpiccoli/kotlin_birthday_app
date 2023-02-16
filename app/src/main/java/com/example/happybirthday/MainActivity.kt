33f336a3e28a5f5e740e5e8f03f1fcfdb01acb53
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-02-16 17:11:46 -0300

    feat: fullscreen

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -27,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                BirthdayGreetingWithImage(message = "Happy Birthday Pamela!", from = "-from Gustavo")
+                    BirthdayGreetingWithImage(message = "Happy Birthday Pamela!", from = "-from Gustavo")
                 }
             }
         }
@@ -36,17 +35,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthDayGreetingWithText(message: String, from: String){
     Column{
-        Text(text = message, fontSize = 26.sp)
+        Text(text = message, fontSize = 26.sp,
+        modifier = Modifier.fillMaxWidth())
         Text(text = from, fontSize = 14.sp)
     }
 }
 @Composable
 fun BirthdayGreetingWithImage(message: String, from: String){
     val image = painterResource(id = R.drawable.androidparty)
-  Box {
-      Image(painter = image, contentDescription = null )
-      BirthDayGreetingWithText(message = message, from = from)
-  }
+    Box {
+        Image(painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            contentScale = ContentScale.Crop
+        )
+        BirthDayGreetingWithText(message = message, from = from)
+    }
 }
 @Preview(showBackground = false)
 @Composable
